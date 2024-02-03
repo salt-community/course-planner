2be24c6c03478a9a9025c6c5fc72b54afa3998df
Marcus Rådell <marcus@radell.net>
2024-02-03 19:05:17 +0100

    add empty course list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,41 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Create Course</h1>
-      <fieldset>
-        <label htmlFor="course-start">Start</label>
-        <input type="date" name="course-start" id="course-start" />
-      </fieldset>
+      <h1>Courses</h1>
 
-      <fieldset>
-        <div className="flex flex-col">
-          <legend>Program</legend>
+      <section>
+        <h2>Add Course</h2>
 
-          <span>
-            <input value="jsfs" id="jsfs" type="radio" name="program" />
-            <label htmlFor="jsfs">JS</label>
-          </span>
-          <span>
-            <input value="dnfs" id="dnfs" type="radio" name="program" />
-            <label htmlFor="dnfs">.NET</label>
-          </span>
-          <span>
-            <input value="jfs" id="jfs" type="radio" name="program" />
-            <label htmlFor="jfs">Java</label>
-          </span>
-        </div>
-      </fieldset>
+        <fieldset>
+          <label htmlFor="course-start">Start</label>
+          <input type="date" name="course-start" id="course-start" />
+        </fieldset>
+
+        <fieldset>
+          <div className="flex flex-col">
+            <legend>Program</legend>
+
+            <span>
+              <input value="jsfs" id="jsfs" type="radio" name="program" />
+              <label htmlFor="jsfs">JS</label>
+            </span>
+            <span>
+              <input value="dnfs" id="dnfs" type="radio" name="program" />
+              <label htmlFor="dnfs">.NET</label>
+            </span>
+            <span>
+              <input value="jfs" id="jfs" type="radio" name="program" />
+              <label htmlFor="jfs">Java</label>
+            </span>
+          </div>
+        </fieldset>
+      </section>
+
+      <section>
+        <h2>Course list</h2>
+
+        <p>No courses found.</p>
+      </section>
     </main>
   );
 }
