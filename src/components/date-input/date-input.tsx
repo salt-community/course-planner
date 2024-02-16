10737233a4c7ed39469d014a934e9d30301741c7
alexmiloeski <a.miloeski@gmail.com>
2024-02-16 13:54:40 +0100

    replace custom calendar element with html date input

--- src/components/date-input/date-input.tsx
+++ src/components/date-input/date-input.tsx
@@ -12,12 +12,7 @@ export const DateInput = ({ dateState }: Props) => {
   const { date, setDate, hydrated } = dateState;
   return (
     hydrated && (
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border w-[280px]"
-      />
+      <input type="date" value={date?.toString()} onChange={e => setDate(new Date(e.target.value))} />
     )
   );
 };
